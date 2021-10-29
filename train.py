828686b3f9910e101b87425624987f98f4293c84
Sudip Ghosh <60208804+sudipg4112001@users.noreply.github.com>
2021-10-30 00:30:21 +0530

    Update train.py

--- train.py
+++ train.py
@@ -1,27 +1,18 @@
-import tensorflow as tf  # deep learning library. Tensors are just multi-dimensional arrays
+import tensorflow as tf  
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Flatten
 from keras.layers import Conv2D, MaxPooling2D
 import keras
 
-mnist = tf.keras.datasets.mnist  # mnist is a dataset of 28x28 images of handwritten digits and their labels
-(x_train, y_train),(x_test, y_test) = mnist.load_data()  # unpacks images to x_train/x_test and labels to y_train/y_test
+mnist = tf.keras.datasets.mnist 
+(x_train, y_train),(x_test, y_test) = mnist.load_data() 
 
 
 x_train = x_train.reshape(x_train.shape[0], 28, 28, 1)
 x_test = x_test.reshape(x_test.shape[0], 28, 28, 1)
 
-x_train = tf.keras.utils.normalize(x_train, axis=1)  # scales data between 0 and 1
-x_test = tf.keras.utils.normalize(x_test, axis=1)  # scales data between 0 and 1
-
-#model = tf.keras.models.Sequential()  # a basic feed-forward model
-#model.add(tf.keras.layers.Flatten())  # takes our 28x28 and makes it 1x784
-#model.add(tf.keras.layers.Dense(128, activation=tf.nn.relu))  # a simple fully-connected layer, 128 units, relu activation
-#model.add(tf.keras.layers.Dense(128, activation=tf.nn.relu))  # a simple fully-connected layer, 128 units, relu activation
-#model.add(tf.keras.layers.Dense(10, activation=tf.nn.softmax))  # our output layer. 10 units for 10 classes. Softmax for probability distribution
-
-# 6. Preprocess class labels
-# convert class vectors to binary class matrices
+x_train = tf.keras.utils.normalize(x_train, axis=1) 
+x_test = tf.keras.utils.normalize(x_test, axis=1)  
 y_train = keras.utils.to_categorical(y_train, 10)
 y_test = keras.utils.to_categorical(y_test, 10)
 
@@ -37,13 +28,13 @@ model.add(Dense(128, activation='relu'))
 model.add(Dropout(0.5))
 model.add(Dense(10, activation='softmax'))
 
-model.compile(optimizer='Adadelta',  # Good default optimizer to start with
-              loss='categorical_crossentropy',  # how will we calculate our "error." Neural network aims to minimize loss.
-              metrics=['accuracy'])  # what to track
+model.compile(optimizer='Adadelta', 
+              loss='categorical_crossentropy',  
+              metrics=['accuracy']) 
 
-model.fit(x_train, y_train, epochs=10)  # train the model
+model.fit(x_train, y_train, epochs=10) 
 
-val_loss, val_acc = model.evaluate(x_test, y_test)  # evaluate the out of sample data with model
-print(val_loss)  # model's loss (error)
-print(val_acc)  # model's accuracy
+val_loss, val_acc = model.evaluate(x_test, y_test)
+print(val_loss)  
+print(val_acc)  
 model.save('mymodel2.h5')
